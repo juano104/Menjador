f792c40cd4e827cefe6b7038fff79057156d5c5c
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-01-20 09:46:17 +0100

    proba

---
+++ config/Database.php
@@ -0,0 +1,21 @@
+<?php
+class Database{
+
+    private $host = "40.68.231.216:3306";
+    private $username = "menjador";
+    private $password = "password";
+    private $db = "api";
+    public $conn;
+
+    public function connect() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        } catch (PDOException $exception) {
+            echo "Database could not be connected: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+    
+}
