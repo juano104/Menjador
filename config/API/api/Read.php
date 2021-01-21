9b6eb61995ed8b91b8d65403a7b1af6d9f7eb349
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-01-21 11:26:54 +0100

    Update Read.php

--- config/API/api/Read.php
+++ config/API/api/Read.php
@@ -25,16 +25,16 @@ if ($count > 0) {
     $userArr = array();
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $arr = array(extract($row));
-        /*$e = array(
+        extract($row);
+        $e = array(
             "name" => $name,
             "last_name" => $last_name,
             "DNI" => $DNI,
             "birth_date" => $birth_date,
             "role" => $role,
         );
-*/
-        array_push($userArr, $arr);
+
+        array_push($userArr, $e);
     }
     echo json_encode($userArr);
 }
