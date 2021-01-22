b0775a34dfe2d938ee192b0e1cc51c0f8e0196ed
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-01-22 11:42:50 +0100

    proba One

--- config/API/api/ReadOne.php
+++ config/API/api/ReadOne.php
@@ -12,21 +12,27 @@ $db_conn = $db->connect();
 $user = new Person($db_conn);
 
 $user->setName(isset($_GET["name"]) ? $_GET["name"] : die());
-$user->readOne();
+$stmt = $user->readOne();
+$count = $stmt->rowCount();
 
-if ($user->getName() != null) {
-    // create array
-    $user_arr = array(
-        "name" => $name,
-        "last_name" => $last_name,
-        "DNI" => $DNI,
-        "birth_date" => $birth_date,
-        "role" => $role,
-    );
+//echo json_encode($count);
 
-    http_response_code(200);
-    echo json_encode($user_arr);
-} else {
-    http_response_code(404);
-    echo json_encode("Person not found.");
+
+if ($count > 0) {
+
+    $userArr = array();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $e = array(
+            "name" => $name,
+            "last_name" => $last_name,
+            "DNI" => $DNI,
+            "birth_date" => $birth_date,
+            "role" => $role,
+        );
+
+        array_push($userArr, $e);
+    }
+    //echo json_encode($userArr);
 }
