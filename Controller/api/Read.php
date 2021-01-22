3061834982aa7603e0ba87db57a71ad0f6a2c7cf
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-01-22 13:03:42 +0100

    Make MVC

---
+++ Controller/api/Read.php
@@ -0,0 +1,38 @@
+<?php
+
+//Headers
+include_once '../../Model/Database.php';
+include_once '../../Model/Person.php';
+
+//DB
+$db = new Database();
+$db_conn = $db->connect();
+
+//User
+$user = new Person($db_conn);
+
+$stmt = $user->read();
+$count = $stmt->rowCount();
+
+//echo json_encode($count);
+
+
+if ($count > 0) {
+
+    $userArr = array();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $e = array(
+            "name" => $name,
+            "last_name" => $last_name,
+            "DNI" => $DNI,
+            "birth_date" => $birth_date,
+            "role" => $role,
+        );
+
+        array_push($userArr, $e);
+    }
+    //echo json_encode($userArr);
+}
+include "../../View/View.php";
---
+++ Controller/api/ReadOne.php
@@ -0,0 +1,30 @@
+<?php
+
+//Headers
+include_once '../../Model/Database.php';
+include_once '../../Model/Person.php';
+
+//DB
+$db = new Database();
+$db_conn = $db->connect();
+
+//User
+$user = new Person($db_conn);
+
+$user->setDNI(isset($_GET["DNI"]) ? $_GET["DNI"] : die());
+$user->readOne();
+$userArr = array();
+
+if ($user->getName() != null) {
+    // create array
+    $e = array(
+        "name" => $user->getName(),
+        "last_name" => $user->getLast_name(),
+        "DNI" => $user->getDNI(),
+        "birth_date" => $user->getBirth_date(),
+        "role" => $user->getRole(),
+    );
+    array_push($userArr, $e);
+    //echo json_encode($userArr);
+}
+include "../../View/View.php";
