f886d83b1cfd1fb4bf8ac4ac622c0c55e3bd1844
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-01-25 12:39:24 +0100

    fix read

--- View/View.php
+++ View/View.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+<!-- Hacer un view para cada read?? -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Read_All</title>
