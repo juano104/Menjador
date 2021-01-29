9d60fea2326409999237c5381e8b3dc72e3b1f90
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-01-29 12:39:51 +0100

    Update View-Admin.php

--- View/User_Admin/View-Admin.php
+++ View/User_Admin/View-Admin.php
@@ -85,11 +85,10 @@
 
     </div>
 
-    <div class="container" style="margin-bottom: 10%; margin-top: 10%;">
+    <div class="container" style="margin-bottom: 10%; margin-top: 5%;">
         <h1>Taula Pares</h1>
         
         <table id="table" class="table table-striped table-bordered">
-        <button class='btn btn-success' name='afegirpare' data-toggle='modal' data-target='#ModalPare'><i class='fas fa-plus-circle'></i></button>
             <thead>
                 <tr>
                     <th>Name</th>
