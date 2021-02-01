e455695555ad27285ecd0bdef6444665b6202c90
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-02-01 09:59:11 +0100

    Update Student.php

--- Model/Student.php
+++ Model/Student.php
@@ -100,6 +100,6 @@ class Student
         $stmt->bindParam(1, $this->$student_ID);
         $stmt->bindParam(2, $this->$allergy_ID);
 
-        $stmt->execute();
+        if(!$stmt->execute()) echo $stmt->error;
     }
 }
