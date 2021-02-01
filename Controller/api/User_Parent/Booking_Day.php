57e5e0e200ec2c2ff6a4f48733ed8c03365db938
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-01 19:39:34 +0100

    fixes

--- Controller/api/User_Parent/Booking_Day.php
+++ Controller/api/User_Parent/Booking_Day.php
@@ -7,30 +7,32 @@ include_once '../../../Model/User_Parent.php';
 //DB
 $db = new Database();
 $db_conn = $db->connect();
-if (isset($_POST["submit"])) {
+//if (isset($_POST["submit"])) {
     //User
     $parent = new User_Parent($db_conn);
 
     //id, start date, end date
     $parent->setStudent_ID($_POST["idstudent"]);
     $parent->setStart_date($_POST["date"]);
-    $parent->setEnd_Date($parent->getStart_date());
+    $parent->setEnd_Date($_POST["date"]);
 
-
-    if ($parent->makeReservation()) {
-        //echo json_encode(array("statusCode" => 200));
-        $parent->setBooking_ID($db_conn->lastInsertId());
-        echo json_encode("Made a Booking reservation!");
-        if ($parent->makeDayReservation()) {
-            echo json_encode("Made reservation for ONE day!");
-        }
-
-        //$last_id = $db_conn->lastInsertId();
-    } else {
-        /* echo $_POST["nomalumne"];
+    if ($parent->getStudent_ID() != "") {
+        if ($parent->makeReservation()) {
+            $parent->setBooking_ID($db_conn->lastInsertId());
+            echo json_encode("Made a Booking reservation!");
+            if ($parent->makeDayReservation()) {
+                echo json_encode("Made reservation for ONE day!");
+            }
+        } else {
+            /* echo $_POST["nomalumne"];
       echo $_POST["pareID"]; */
-        echo json_encode("Error");
-        //echo json_encode("ERROR");
+            echo json_encode("Error");
+            //echo json_encode("ERROR");
+        }
     }
-}
+
+
+    //$last_id = $db_conn->lastInsertId();
+
+//}
 //header("Location: http://www.menjadorescola.me/Menjador/Controller/api/User_Admin/Insert/Insert_Student.php");
