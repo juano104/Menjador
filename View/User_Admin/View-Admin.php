17bd0b660f0d2662b8c4978fda04d554b466cacc
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-02-01 18:34:36 +0100

    Update View-Admin.php

--- View/User_Admin/View-Admin.php
+++ View/User_Admin/View-Admin.php
@@ -124,7 +124,7 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <input type="submit" id="afegir" class="btn btn-success" value="Afegir Pare">
                     </div>
                 </form>
             </div>
