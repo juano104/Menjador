d3fe5a2f099aa4a35a4eb51b3f405e143d8f758d
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-01 22:52:20 +0100

    Update Booking_Day.php

--- Controller/api/User_Parent/Booking_Day.php
+++ Controller/api/User_Parent/Booking_Day.php
@@ -11,7 +11,7 @@ $db_conn = $db->connect();
 
 $parent = new User_Parent($db_conn);
 //$redir = $parent->setUsername(isset($_GET["username"]) ? $_GET["username"] : die());
-$properties = json_decode(file_get_contents("php://input"));
+$properties = json_decode(file_get_contents("../../../View/User_Parent/Home_Parent.php"));
 
 $parent->setStart_date($properties->date);
 $parent->setEnd_date($properties->date);
