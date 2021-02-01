746a68707771d35be1a98b6a928a5d2614cc3b7e
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-01 20:25:46 +0100

    Update Booking_Day.php

--- Controller/api/User_Parent/Booking_Day.php
+++ Controller/api/User_Parent/Booking_Day.php
@@ -1,11 +1,5 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
 //Headers
 include_once '../../../Model/Database.php';
 include_once '../../../Model/User_Parent.php';
@@ -19,7 +13,7 @@ if (isset($_POST["submit"])) {
     $parent = new User_Parent($db_conn);
     //$properties = json_decode(file_get_contents("php://input"));
 
-    
+
     $parent->setStart_date($_POST['date']);
     $parent->setEnd_date($_POST['date']);
     $parent->setStudent_ID($_POST['idstudent']);
@@ -28,6 +22,7 @@ if (isset($_POST["submit"])) {
     if ($parent->makeReservation()) {
         //json_encode("Made reservation");
         echo json_encode(array("statusCode bad RES" => 201));
+        $parent->setBooking_ID($db_conn->lastInsertId());
         if ($parent->makeDayReservation()) {
             //json_encode("Made DAY reservation");
             echo json_encode(array("statusCode" => 200));
