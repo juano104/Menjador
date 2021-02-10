ca9f8a5825a2195206211240956821fb016ef788
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-10 12:49:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="api/Controller/Parent/Read_Child.php" method="get">
+    <form action="api/Controller/Student/Read.php" method="get">
         <button type="submit" name="username" value="56142879E">Parent: Fulanito Fulano</button>
     </form>
-    <form action="api/Controller/Parent/Read_Child.php" method="get">
+    <form action="api/Controller/Student/Read.php" method="get">
         <button type="submit" name="username" value="79481024P">Parent: Marc Jaume</button>
     </form>
     <br>
