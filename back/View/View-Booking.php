0a1ab3a6ee0b1d4347abd5b68350887ae1e2cbc0
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-02-12 11:44:38 +0100

    Update View-Booking.php

--- back/View/View-Booking.php
+++ back/View/View-Booking.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/select/1.2.7/css/select.bootstrap4.min.css" />
 
     <!--<link rel="stylesheet" href="../public/css/estils.css">-->
-    <link rel="stylesheet" href="../../back/public/css/estils.css"">
+    <link rel="stylesheet" href="../../back/public/css/esls.css">
 
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
@@ -45,47 +45,49 @@
 
 <body>
 
-    <div class="container-fluid">
-
-        <!-- Menu de navegacio -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
-
-            <!-- LOGO -->
-
-            <!--<a href="Pagina-Principal.html" class="navbar-brand">
-                <img src="../../../../View/User_Admin/logo.png" alt="LogoImatge" class="d-inline-block align-middle imgres">
-            </a>-->
-
-            <!-- LOGO -->
-
-
-            <button class="navbar-toggler botores" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="Pagina-Principal.html"><i class="fas fa-home"></i>
-                            <span class="sr-only">(current)</span>
-                        </a>
-                    </li>
-                    <!-- Dropdown -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#"><i class="fas fa-user"></i></a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="Pagina-Montanya.html">Guitarres</a>
-                            <a class="dropdown-item" href="Pagina-Carretera.html">Flautes</a>
-                            <a class="dropdown-item" href="Pagina-Paseig.html">Altaveus</a>
-                        </div>
-                    </li>
-
-                </ul>
-
-            </div>
-        </nav>
-        <!-- Menu de navegacio -->
-
-    </div>
+<div class="container-fluid">
+            <!-- Menu de navegacio -->
+            <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
+                <!-- LOGO -->
+                <a href="Pagina-Principal.html" class="navbar-brand">
+                    <img src="front/public/img/lo.png" alt="" class="d-inline-block align-middle imgres">
+                </a>
+                <!-- LOGO -->
+                <button
+                    class="navbar-toggler botores"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarResponsive"
+                    aria-controls="navbarResponsive"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="Pagina-Principal.html">
+                                <i class="fas fa-home"></i>
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <!-- Dropdown -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">
+                                <i class="fas fa-user"></i>
+                            </a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="Pagina-Montanya.html">Guitarres</a>
+                                <a class="dropdown-item" href="Pagina-Carretera.html">Flautes</a>
+                                <a class="dropdown-item" href="Pagina-Paseig.html">Altaveus</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- Menu de navegacio -->
+        </div>
 
     <div class="container" style="margin-bottom: 10%; margin-top: 5%;">
         <h1>Reserves de un dia</h1>
@@ -218,84 +220,72 @@
         });
     </script>
     <footer class="page-footer font-small stylish-color-dark pt-4">
-
-        <!-- Footer Links -->
-        <div class="container text-center text-md-left">
-
-            <!-- Grid row -->
-            <div class="row">
-
-                <hr class="clearfix w-100 d-md-none">
-
-                <!-- Grid column -->
-                <div class="col-md-2 mx-auto">
-
-                    <!-- Links -->
-                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#!">Link 1</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 2</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 3</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 4</a>
-                        </li>
-                    </ul>
-
-                </div>
-                <!-- Grid column -->
-                <div class="col-md-4 mx-auto">
-
-                    <!-- Content -->
-                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Footer Content</h5>
-                    <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+            <!-- Footer Links -->
+            <div class="container text-center text-md-left">
+                <!-- Grid row -->
+                <div class="row">
+                    <hr class="clearfix w-100 d-md-none">
+                    <!-- Grid column -->
+                    <div class="col-md-2 mx-auto">
+                        <!-- Links -->
+                        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- Grid column -->
+                    <div class="col-md-4 mx-auto">
+                        <!-- Content -->
+                        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Footer Content</h5>
+                        <p>
+                            Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
                         consectetur
-                        adipisicing elit.</p>
-
-                </div>
-
-                <!-- Social buttons -->
-                <div class="col-md-2 mx-auto">
-                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4 text-center">Xarxes</h5>
-                    <br>
-                    <ul class="list-unstyled list-inline text-center">
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-fb mx-1">
-                                <i class="fab fa-facebook-f"> </i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-tw mx-1">
-                                <i class="fab fa-twitter"> </i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-gplus mx-1">
-                                <i class="fab fa-google-plus-g"> </i>
-                            </a>
-                        </li>
-
-                    </ul>
+                        adipisicing elit.
+                        </p>
+                    </div>
                     <!-- Social buttons -->
-
+                    <div class="col-md-2 mx-auto">
+                        <h5 class="font-weight-bold text-uppercase mt-3 mb-4 text-center">Xarxes</h5>
+                        <br>
+                        <ul class="list-unstyled list-inline text-center">
+                            <li class="list-inline-item">
+                                <a class="btn-floating btn-fb mx-1">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="btn-floating btn-tw mx-1">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a class="btn-floating btn-gplus mx-1">
+                                    <i class="fab fa-google-plus-g"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <!-- Social buttons -->
+                    </div>
                 </div>
-
-            </div>
-            <!-- Grid row -->
-
-            <!-- Copyright -->
-            <div class="footer-copyright text-center py-3">� 2020 Copyright:
-                <a href="https://mdbootstrap.com/">Music</a>
-            </div>
-            <!-- Copyright -->
-
-    </footer>
+                <!-- Grid row -->
+                <!-- Copyright -->
+                <div class="footer-copyright text-center py-3">
+                    © 2020 Copyright:
+                    <a href="https://mdbootstrap.com/">Music</a>
+                </div>
+                <!-- Copyright -->
+            </footer>
     <!-- Footer -->
 
 </body>
