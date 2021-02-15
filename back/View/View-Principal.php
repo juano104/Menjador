2d2bf7e6f915e46933fa0bfc32a35153e9c4621b
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-15 10:17:54 +0100

    update

--- back/View/View-Principal.php
+++ back/View/View-Principal.php
@@ -2,45 +2,71 @@
 <html lang="en">
 
 <head>
+    <!-- Hacer un view para cada read?? -->
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Principal</title>
+
+    <!-- css -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css" />
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" />
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/select/1.2.7/css/select.bootstrap4.min.css" />
+
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+
+    <!-- js -->
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
+    <script src="https://cdn.datatables.net/select/1.2.7/js/dataTables.select.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-danger static-top">
-        <!-- LOGO -->
-        <a href="Pagina-Principal.html" class="navbar-brand">
-            <img src="front/public/img/lo.png" alt="" class="d-inline-block align-middle imgres">
-        </a>
-        <!-- LOGO -->
-        <button class="navbar-toggler botores" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="Pagina-Principal.html">
-                        <i class="fas fa-home"></i>
-                        <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <!-- Dropdown -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fas fa-user"></i>
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="Pagina-Montanya.html">Guitarres</a>
-                        <a class="dropdown-item" href="Pagina-Carretera.html">Flautes</a>
-                        <a class="dropdown-item" href="Pagina-Paseig.html">Altaveus</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <h2>Aqui se verian las reservas del dia elegido...</h2>
+
+    <div class="container-fluid">
+
+        <!-- Menu de navegacio -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-danger static-top">
+
+            <button class="navbar-toggler botores" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="Pagina-Principal.html"><i class="fas fa-home"></i>
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </nav>
+        <!-- Menu de navegacio -->
+
+    </div>
+    <h2>Aqui se ven las reservas de un dia en concreto</h2>
+
 </body>
 
 </html>
