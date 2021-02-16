9fe103156355b4a3a6e770d8688bf933f7d809d4
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-16 10:12:49 +0100

    Update routes.php

--- intranet/core/routes.php
+++ intranet/core/routes.php
@@ -10,7 +10,8 @@ $router->define([
     "" => "View/index.html",
     //
     "principal" => "../api/Controller/Plate/Read.php",
-    "datos" => "../api/Controller/Menu/Read-MenuPlate.php",
+    "datos?start=2021-02-01&end=2021-03-01&_=1613466702873" => "../api/Controller/Menu/Read-MenuPlate.php",
+    //"datos?start=2021-02-01&end=2021-03-01&_=1613466702873",
     "insertar" => "View/View-Insert-Plate.php",
     "insertar/plate" => "../api/Controller/Plate/Insert.php",
     "insertar/menu" => "../api/Controller/Menu/Insert.php"
