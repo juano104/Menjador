03e8ed787889cbbb717bc18e691c7ef495c6002b
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-17 12:27:31 +0100

    Update index.php

--- front/View/index.php
+++ front/View/index.php
@@ -33,7 +33,7 @@
 
 <body>
     <!-- Menu de navegacio -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-success static-top">
         <!-- LOGO -->
         <a href="Pagina-Principal.html" class="navbar-brand">
             <img src="public/img/logo.png" alt="" class="d-inline-block align-middle imgres">
@@ -45,20 +45,25 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="reservas">
-                        Mis Reservas
+                    <a class="nav-link" href="http://menjadorescola.me">
+                        <i class="fas fa-home"></i>
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        Cerrar Sesion
+                <!-- Dropdown -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fas fa-user"></i>
                     </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="Pagina-Montanya.html">Guitarres</a>
+                        <a class="dropdown-item" href="Pagina-Carretera.html">Flautes</a>
+                        <a class="dropdown-item" href="Pagina-Paseig.html">Altaveus</a>
+                    </div>
                 </li>
             </ul>
         </div>
     </nav>
-    <!-- Menu de navegacio -->
     <br>
 
     <form>
