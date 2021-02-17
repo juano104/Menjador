e79aec2a254ba23e4bc9e331af1cad7bc39d3895
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-17 12:57:19 +0100

    Update index.php

--- front/View/index.php
+++ front/View/index.php
@@ -32,15 +32,11 @@
 
 <body>
     <!-- Menu de navegacio -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-success static-top">
+    <nav class="navbar navbar-expand-lg navbar-light static-top">
         <!-- LOGO -->
         <a href="Pagina-Principal.html" class="navbar-brand">
             <img src="public/img/logo.png" alt="" class="d-inline-block align-middle imgres">
         </a>
-        <!-- LOGO -->
-        <button class="navbar-toggler botores" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
