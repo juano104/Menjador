965ecc8d3f1cfe3a56bfcbd2f64618501b363d91
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-02-17 20:55:55 +0100

    Update index.php

--- front/View/index.php
+++ front/View/index.php
@@ -9,21 +9,16 @@
     <link rel="stylesheet" href="public/css/estils.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script >
-        var jQuery_3_5_1 = jQuery_3_5_1.noConflict(true);
-    </script>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="public/js/jquery.min.js"></script>
+    <script>var $j = jQuery.noConflict(true);</script>
     <!--<link rel="stylesheet" href="http://code.jquery.com/ui/1.8.3/themes/base/jquery-ui.css" />
     <script type="text/javascript" src="http://code.jquery.com/ui/1.8.3/jquery-ui.js"></script>-->
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 
-    <script type="text/javascript" src="public/js/jquery.min.js"></script>
-    <script type="text/javascript">
-        var jQuery_3_3_1 = jQuery_3_3_1.noConflict(true);
-    </script>
+
     <script src="public/js/moment.min.js"></script>
     <script src="public/js/fullcalendar.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
