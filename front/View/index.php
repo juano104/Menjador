d3d92e37d6d2629f98e1a1a7d9590419acd27dbd
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-17 12:12:04 +0100

    styles

--- front/View/index.php
+++ front/View/index.php
@@ -44,21 +44,15 @@
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="Pagina-Principal.html">
-                            <i class="fas fa-home"></i>
+                        <a class="nav-link" href="reservas">
+                            Mis Reservas
                             <span class="sr-only">(current)</span>
                         </a>
                     </li>
-                    <!-- Dropdown -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">
-                            <i class="fas fa-user"></i>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Cerrar Sesion
                         </a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="Pagina-Montanya.html">Guitarres</a>
-                            <a class="dropdown-item" href="Pagina-Carretera.html">Flautes</a>
-                            <a class="dropdown-item" href="Pagina-Paseig.html">Altaveus</a>
-                        </div>
                     </li>
                 </ul>
             </div>
