61aae98f06271122c85ad3189a25883574f26a32
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-17 13:09:49 +0100

    Update index.php

--- front/View/index.php
+++ front/View/index.php
@@ -17,10 +17,6 @@
 
     <!--<link rel="stylesheet" href="http://code.jquery.com/ui/1.8.3/themes/base/jquery-ui.css" />
     <script type="text/javascript" src="http://code.jquery.com/ui/1.8.3/jquery-ui.js"></script>-->
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script>
         $(function() {
             $(".datepicker").datepicker({
