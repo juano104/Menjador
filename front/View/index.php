a848d26bbe5be3faccc39fe3d0b7e445185f9cb8
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-17 13:14:50 +0100

    Update index.php

--- front/View/index.php
+++ front/View/index.php
@@ -9,10 +9,6 @@
     <link rel="stylesheet" href="public/css/estils.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-
-
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <!--<link rel="stylesheet" href="http://code.jquery.com/ui/1.8.3/themes/base/jquery-ui.css" />
