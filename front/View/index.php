fc33c7f6f6c37eb5954d592234be73d7e46ae339
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-17 12:35:11 +0100

    styles

--- front/View/index.php
+++ front/View/index.php
@@ -1,4 +1,4 @@
-<html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -632,10 +632,10 @@
             <!-- Copyright -->
     </footer>
     <!-- Footer -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+    -->
 
 </body>
 
