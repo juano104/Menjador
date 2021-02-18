1a982fdcead5c318cd2e8eebb97a82a45c0d93d2
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-02-18 09:26:02 +0100

    Update index.php

--- front/View/index.php
+++ front/View/index.php
@@ -679,7 +679,7 @@
                     var imgData = canvas.toDataURL(
                         'image/png');
                     var doc = new jsPDF('p', 'mm');
-                    doc.addImage(imgData, 'PNG', 20, 3);
+                    doc.addImage(imgData, 'PNG', -1, 10);
                     doc.save('Menu.pdf');
                 }
             });
