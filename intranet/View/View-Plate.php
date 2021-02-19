abd9abe8c0be638d2b622497f63c9c3c91e37358
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-02-19 12:11:15 +0100

    estls

--- intranet/View/View-Plate.php
+++ intranet/View/View-Plate.php
@@ -22,20 +22,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
 
   <style>
-    .container {
-      margin-top: 5%;
-    }
-
-    #calendar {
-      max-width: 100%;
-      margin-bottom: 5%;
-    }
-
-    #calendar th {
-      color: white;
-      border: black 1px solid;
-      background-color: #264653;
-    }
+    
   </style>
 </head>
 
