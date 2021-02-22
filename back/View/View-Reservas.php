15d8368ba3cc04fcb8fe2a9e9c6254146aabbf99
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-22 19:07:05 +0100

    Update View-Reservas.php

--- back/View/View-Reservas.php
+++ back/View/View-Reservas.php
@@ -50,6 +50,19 @@
             color: red;
         }
     </style>
+
+    <script>
+        $(document).ready(function() {
+            var date_input = $('input[name="day"]'); //our date input has the name "date"
+            var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+            date_input.datepicker({
+                format: 'yyyy/mm/dd',
+                container: container,
+                todayHighlight: true,
+                autoclose: true,
+            })
+        })
+    </script>
 </head>
 
 <body>
@@ -62,13 +75,10 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-6 col-sm-6 col-xs-12">
-                    <form action="https://formden.com/post/MlKtmY4x/" class="form-horizontal" method="post">
+                    <form action="" class="form-horizontal" method="post">
                         <div class="form-group ">
                             <label class="control-label col-sm-2 requiredField" for="date">
                                 Date
-                                <span class="asteriskField">
-                                    *
-                                </span>
                             </label>
                             <div class="col-sm-10">
                                 <div class="input-group">
@@ -114,14 +124,6 @@
 
     <script>
         $(document).ready(function() {
-            var date_input = $('input[name="day"]'); //our date input has the name "date"
-            var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
-            date_input.datepicker({
-                format: 'yyyy/mm/dd',
-                container: container,
-                todayHighlight: true,
-                autoclose: true,
-            })
             //DT
             var t = $('#tableReservas').DataTable({
                 "bPaginate": false,
