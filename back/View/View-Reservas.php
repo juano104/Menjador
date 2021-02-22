9420052c22e478e74073cca61ce7c0e190e3158d
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-22 12:47:44 +0100

    Update View-Reservas.php

--- back/View/View-Reservas.php
+++ back/View/View-Reservas.php
@@ -11,6 +11,26 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css">
   
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js"></script>
+    
+    <!-- js -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
+    <script src="https://cdn.datatables.net/select/1.2.7/js/dataTables.select.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
+    
+    
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script>
@@ -21,17 +41,18 @@
 
         });
     </script>
+
 </head>
 
 <body>
-    <form action="view/byday" method="POST">
+    <form action="" method="POST">
         <input name="day" type="text" id="datepicker" autocomplete="off">
         <label for="datepicker">Date</label>
         <input type="submit" value="SUBMIT">
     </form>
 
     <div class="container" style="margin-bottom: 10%; margin-top: 5%;">
-        <h1>Reservas(<?php echo $booking->getDate() ?>)</h1>
+        <h1>Reservas para <?php echo $booking->getDate() ?></h1>
         <table id="tableReservas" class="table table-striped table-bordered">
             <thead>
                 <tr>
