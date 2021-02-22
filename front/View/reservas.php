22fe3e18a23283cc888df78c87ea1fcea063a821
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-22 10:28:23 +0100

    POST DAY

--- front/View/reservas.php
+++ front/View/reservas.php
@@ -35,8 +35,8 @@
 
     <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
     
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <!--<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>-->
 
     <script>
         $(function() {
