8bc32cf26d29f639e02532626395ff690ac43415
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-22 12:53:01 +0100

    Update View-Reservas.php

--- back/View/View-Reservas.php
+++ back/View/View-Reservas.php
@@ -5,10 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TOTAL BY DAY</title>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css">
+    <title>Mis Reservas</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css" />
 
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" />
@@ -19,12 +16,7 @@
 
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
-    <script>
-        var $j = jQuery.noConflict(true);
-    </script>
-  
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js"></script>
-    
+
     <!-- js -->
 
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
@@ -43,9 +35,9 @@
 
     <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
     
-    
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <!--<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>-->
+
     <script>
         $(function() {
             $("#datepicker").datepicker({
@@ -54,31 +46,35 @@
 
         });
     </script>
-
 </head>
 
 <body>
-    <form action="" method="POST">
-        <input name="day" type="text" id="datepicker" autocomplete="off">
-        <label for="datepicker">Date</label>
-        <input type="submit" value="SUBMIT">
-    </form>
+    <div class="container-fluid">
+        <!-- Menu de navegacio -->
+        <?php require_once "navbar.php" ?>
+        <!-- Menu de navegacio -->
+    </div>
+    <div class="container">
+        <form action="" method="POST">
+            <input name="day" type="text" id="datepicker" autocomplete="off">
+            <label for="datepicker">Date</label>
+            <input type="submit" value="SUBMIT">
+        </form>
+    </div>
 
     <div class="container" style="margin-bottom: 10%; margin-top: 5%;">
-        <h1>Reservas para <?php echo $booking->getDate() ?></h1>
+        <h1>Mis Reservas(<?php echo $booking->getDate() ?>)</h1>
         <table id="tableReservas" class="table table-striped table-bordered">
             <thead>
                 <tr>
                     <th>ID</th>
                     <th>Name</th>
                     <th>Last Name</th>
-                    <th>Allergies</th>
+                    <th>AlLergy</th>
                 </tr>
             </thead>
         </table>
     </div>
-
-
 
 
 
@@ -91,12 +87,12 @@
                 "bFilter": true,
                 "bInfo": false,
                 "bAutoWidth": false,
-                data: <?php echo json_encode($arrday); ?>,
+                data: <?php echo json_encode($arrextra); ?>,
                 columns: [{
                     data: 'ID'
                 }, {
                     data: 'name'
-                },{
+                }, {
                     data: 'last_name'
                 }, {
                     data: 'allergy'
@@ -115,12 +111,12 @@
                     "bFilter": true,
                     "bInfo": false,
                     "bAutoWidth": false,
-                    data: <?php echo json_encode($arrday); ?>,
+                    data: <?php echo json_encode($arrextra); ?>,
                     columns: [{
                     data: 'ID'
                 }, {
                     data: 'name'
-                },{
+                }, {
                     data: 'last_name'
                 }, {
                     data: 'allergy'
