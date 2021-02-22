12d0b4f37bcd3c13cf9e8e48e2d39be5fe890c2b
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-22 19:40:28 +0100

    Update View-Reservas.php

--- back/View/View-Reservas.php
+++ back/View/View-Reservas.php
@@ -25,9 +25,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
 
-    <script>
-        $.noConflict(true);
-    </script>
     <!--formden.js communicates with FormDen server to validate fields and submit via AJAX -->
     <script type="text/javascript" src="https://formden.com/static/cdn/formden.js"></script>
     <!-- Special version of Bootstrap that is isolated to content wrapped in .bootstrap-iso -->
@@ -121,7 +118,6 @@
         </table>
     </div>
 
-    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css">
 
