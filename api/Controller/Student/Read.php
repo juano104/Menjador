511a1b63a56a7d5ddf27f00d0fd2bf508a63d3ba
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-23 11:30:48 +0100

    Update Read.php

--- api/Controller/Student/Read.php
+++ api/Controller/Student/Read.php
@@ -32,9 +32,9 @@ if (isset($_POST['submit'])) {
 
 
         include_once "../front/View/index.php";
-    } /*else {
+    } else {
         session_start();
-        if (isset($_SESSION["password"])) {
+        if (isset($_SESSION["name"])) {
             //Headers
             include_once '../api/Model/Database.php';
             include_once '../api/Model/User_Parent.php';
@@ -50,71 +50,10 @@ if (isset($_POST['submit'])) {
             $user->setUsername($_SESSION["username"]);
             $parent_DNI = $user->getUsername();
 
-            $pass = $user->readPassword();
             $stmt = $user->read();
             $count = $stmt->rowCount();
 
-            while ($row = $pass->fetch(PDO::FETCH_ASSOC)) {
-                extract($row);
-                if ($password == $row['password']) {
-                    $_SESSION['password'] = $row['password'];
-                    //header('location:../front/View/index.php');
-                    include_once "../front/View/index.php";
-                } else {
-                    $errorMsg =  "Wrong Email Or Password";
-                }
-            }
+            include_once "../front/View/index.php";
         }
-    }*/
-}
-
-/*
-if (isset($_POST["username"])) {
-    session_start();
-    $_SESSION["username"] = $_POST["username"];
-    //$user = $_SESSION["username"];
-
-    $_SESSION["name"] = "login";
-    //Headers
-    include_once '../api/Model/Database.php';
-    include_once '../api/Model/User_Parent.php';
-    include_once '../api/Model/Student.php';
-
-    //DB
-    $db = new Database();
-    $db_conn = $db->connect();
-
-    //User
-    $user = new User_Parent($db_conn);
-
-    $user->setUsername($_SESSION["username"]);
-    $parent_DNI = $user->getUsername();
-
-    $stmt = $user->read();
-    $count = $stmt->rowCount();
-
-    include_once "../front/View/index.php";
-} else {
-    session_start();
-    if (isset($_SESSION["name"])) {
-        //Headers
-        include_once '../api/Model/Database.php';
-        include_once '../api/Model/User_Parent.php';
-        include_once '../api/Model/Student.php';
-
-        //DB
-        $db = new Database();
-        $db_conn = $db->connect();
-
-        //User
-        $user = new User_Parent($db_conn);
-
-        $user->setUsername($_SESSION["username"]);
-        $parent_DNI = $user->getUsername();
-
-        $stmt = $user->read();
-        $count = $stmt->rowCount();
-
-        include_once "../front/View/index.php";
     }
-}*/
+}
