9328ea4616c1aae450ba9a8c3b398a69a2991441
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-23 11:44:18 +0100

    Update Read.php

--- api/Controller/Student/Read.php
+++ api/Controller/Student/Read.php
@@ -1,12 +1,14 @@
 <?php
+
 if (isset($_POST['submit'])) {
-    if ((isset($_POST['username'])) && (isset($_POST['password']))) {
+    if ((isset($_POST['username']) && $_POST['username'] != '') && (isset($_POST['password']) && $_POST['password'] != '')) {
 
         session_start();
         $username = trim($_POST['username']);
         $passwordf = trim($_POST['password']);
 
         $_SESSION["username"] = $_POST["username"];
+        $_SESSION["username"] = $_POST["password"];
 
         $_SESSION["name"] = "login";
 
@@ -30,28 +32,25 @@ if (isset($_POST['submit'])) {
 
 
         include_once "../front/View/index.php";
-    } else {
-        session_start();
-        if (isset($_SESSION["name"])) {
-            //Headers
-            include_once '../api/Model/Database.php';
-            include_once '../api/Model/User_Parent.php';
-            include_once '../api/Model/Student.php';
+    } else if (isset($_SESSION["username"]) && isset($_SESSION["password"])) {
+        //Headers
+        include_once '../api/Model/Database.php';
+        include_once '../api/Model/User_Parent.php';
+        include_once '../api/Model/Student.php';
 
-            //DB
-            $db = new Database();
-            $db_conn = $db->connect();
+        //DB
+        $db = new Database();
+        $db_conn = $db->connect();
 
-            //User
-            $user = new User_Parent($db_conn);
+        //User
+        $user = new User_Parent($db_conn);
 
-            $user->setUsername($_SESSION["username"]);
-            $parent_DNI = $user->getUsername();
+        $user->setUsername($_SESSION["username"]);
+        $parent_DNI = $user->getUsername();
 
-            $stmt = $user->read();
-            $count = $stmt->rowCount();
+        $stmt = $user->read();
+        $count = $stmt->rowCount();
 
-            include_once "../front/View/index.php";
-        }
+        include_once "../front/View/index.php";
     }
 }
