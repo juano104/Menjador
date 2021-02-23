3cadab562b0ea2b2b5bf279995f0dc0c56ade30c
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-23 10:17:19 +0100

    Update Read.php

--- api/Controller/Student/Read.php
+++ api/Controller/Student/Read.php
@@ -35,40 +35,45 @@ if (isset($_POST['submit'])) {
             extract($row);
             if (password_verify($password, $row['password'])) {
                 $_SESSION['password'] = $row['password'];
-
-                header('location:../front/View/index.php');
-                exit;
+                //header('location:../front/View/index.php');
+                include_once "../front/View/index.php";
             } else {
                 $errorMsg =  "Wrong Email Or Password";
             }
         }
+    } else {
+        session_start();
+        if (isset($_SESSION["password"])) {
+            //Headers
+            include_once '../api/Model/Database.php';
+            include_once '../api/Model/User_Parent.php';
+            include_once '../api/Model/Student.php';
 
-        include_once "../front/View/index.php";
+            //DB
+            $db = new Database();
+            $db_conn = $db->connect();
 
+            //User
+            $user = new User_Parent($db_conn);
 
+            $user->setUsername($_SESSION["username"]);
+            $parent_DNI = $user->getUsername();
 
+            $pass = $user->readPassword();
+            $stmt = $user->read();
+            $count = $stmt->rowCount();
 
-        /*$sqlEmail = "select * from users where email = '" . $email . "'";
-        $rs = mysqli_query($conn, $sqlEmail);
-        $numRows = mysqli_num_rows($rs);
-
-        if ($numRows  == 1) {
-            $row = mysqli_fetch_assoc($rs);
-
-            if (password_verify($password, $row['password'])) {
-                $_SESSION['user_id'] = $row['id'];
-                $_SESSION['first_name'] = $row['first_name'];
-                $_SESSION['last_name'] = $row['last_name'];
-
-                header('location:dashboard.php');
-                exit;
-            } else {
-                $errorMsg =  "Wrong Email Or Password";
+            while ($row = $pass->fetch(PDO::FETCH_ASSOC)) {
+                extract($row);
+                if (password_verify($password, $row['password'])) {
+                    $_SESSION['password'] = $row['password'];
+                    //header('location:../front/View/index.php');
+                    include_once "../front/View/index.php";
+                } else {
+                    $errorMsg =  "Wrong Email Or Password";
+                }
             }
-        } else {
-            $errorMsg =  "No User Found";
         }
-    }*/
     }
 }
 
