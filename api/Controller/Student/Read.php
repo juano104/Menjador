591d0d3efc24fb887720e348d4bb9951717178fc
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-23 08:54:42 +0100

    Update Read.php

--- api/Controller/Student/Read.php
+++ api/Controller/Student/Read.php
@@ -1,29 +1,28 @@
 <?php
 
-if (isset($_POST["username"]) || isset($_SESSION["name"])) {
-    $user = $_POST["username"];
-    session_start();
-    $_SESSION["username"] = $user;
+//if (isset($_POST["username"])) {
+session_start();
+$_SESSION["username"] = $_POST["username"];
+$user = $_SESSION["username"];
 
-    $_SESSION["name"] = "login";
-    //Headers
-    include_once '../api/Model/Database.php';
-    include_once '../api/Model/User_Parent.php';
-    include_once '../api/Model/Student.php';
+$_SESSION["name"] = "login";
+//Headers
+include_once '../api/Model/Database.php';
+include_once '../api/Model/User_Parent.php';
+include_once '../api/Model/Student.php';
 
-    //DB
-    $db = new Database();
-    $db_conn = $db->connect();
+//DB
+$db = new Database();
+$db_conn = $db->connect();
 
-    //User
-    $user = new User_Parent($db_conn);
+//User
+$user = new User_Parent($db_conn);
 
-    //$user->setUsername(isset($_GET["username"]) ? $_GET["username"] : die());
-    $user->setUsername($_SESSION["username"]);
-    $parent_DNI = $user->getUsername();
+$user->setUsername($user);
+$parent_DNI = $user->getUsername();
 
-    $stmt = $user->read();
-    $count = $stmt->rowCount();
+$stmt = $user->read();
+$count = $stmt->rowCount();
 
-    include_once "../front/View/index.php";
-}
+include_once "../front/View/index.php";
+//}
