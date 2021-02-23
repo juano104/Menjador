e6a7debd7c1a4a894b202103af9b06d99c8611fa
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-02-23 09:42:51 +0100

    Update View-Insert-Plate.php

--- intranet/View/View-Insert-Plate.php
+++ intranet/View/View-Insert-Plate.php
@@ -1,47 +1,34 @@
-
 <div class="accordion accordion-flush" id="accordionFlushExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
-      <button
-        class="accordion-button collapsed"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#flush-collapseOne"
-        aria-expanded="false"
-        aria-controls="flush-collapseOne"
-      >
-        Inserir Plat
-      </button>
+      <a class="accordion-button collapsed" type="button" data-mdb-toggle="collapse" data-mdb-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        <i class="far fa-utensils"></i><i class="fas fa-plus-circle"></i>
+      </a>
     </h2>
-    <div
-      id="flush-collapseOne"
-      class="accordion-collapse collapse"
-      aria-labelledby="flush-headingOne"
-      data-mdb-parent="#accordionFlushExample"
-    >
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-mdb-parent="#accordionFlushExample">
       <div class="accordion-body">
-      <div class="container">
-        <h1>INSERIR PLAT</h1>
-        <form action="insertar/plate" class="form-inline" method="post">
-        <div class="form-group mb-2">
-            <label for="nomplat">
+        <div class="container">
+          <h1>INSERIR PLAT</h1>
+          <form action="insertar/plate" class="form-inline" method="post">
+            <div class="form-group mb-2">
+              <label for="nomplat">
 
                 <input type="text" name="nomplat" id="nomplat">
-            </label>
-        </div>
-        <div class="form-group mx-sm-3 mb-2">
-            <label for="tipus">
+              </label>
+            </div>
+            <div class="form-group mx-sm-3 mb-2">
+              <label for="tipus">
                 <select class="custom-select mr-sm-3" name="tipus" id="tipus">
-                    <option selected disabled>Tipus plat...</option>
-                    <option value="1">Primer Plat</option>
-                    <option value="2">Segon Plat</option>
-                    <option value="3">Postre</option>
+                  <option selected disabled>Tipus plat...</option>
+                  <option value="1">Primer Plat</option>
+                  <option value="2">Segon Plat</option>
+                  <option value="3">Postre</option>
                 </select>
-            </label>
-        </div>
+              </label>
+            </div>
             <input type="submit" class="btn btn-secondary mb-2" value="Afegir">
-        </form>
-    </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
