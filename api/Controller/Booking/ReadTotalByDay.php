4a566f37240d7f0c5f0578333fccec285b56076f
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-23 15:58:33 +0100

    Update ReadTotalByDay.php

--- api/Controller/Booking/ReadTotalByDay.php
+++ api/Controller/Booking/ReadTotalByDay.php
@@ -1,19 +1,9 @@
 <?php
 session_start();
 
-//Headers
-include_once '../api/Model/Database.php';
-include_once '../api/Model/Booking.php';
-
-//DB
-$db = new Database();
-$db_conn = $db->connect();
-
-//User
-$booking = new Booking($db_conn);
-
 
 if (isset($_POST['day'])) {
+
     $booking->setDate($_POST["day"]);
 
     $dayname = date('l', strtotime($booking->getDate()));;
@@ -39,6 +29,16 @@ if (isset($_POST['day'])) {
     echo $dayofweek;
     echo $sumd;
 } else {
+    //Headers
+    include_once '../api/Model/Database.php';
+    include_once '../api/Model/Booking.php';
+
+    //DB
+    $db = new Database();
+    $db_conn = $db->connect();
+
+    //User
+    $booking = new Booking($db_conn);
     $today = date("Y-m-d");
     $booking->setDate($today);
 
