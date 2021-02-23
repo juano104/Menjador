0b4a6fb7cfaa3b05e2ef043d24b11d630154d11e
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-23 09:14:48 +0100

    reservas(front)

--- front/View/reservas.php
+++ front/View/reservas.php
@@ -6,12 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mis Reservas</title>
+    <link rel="stylesheet" href="public/css/estils.css">
+
+    <!--DATATABLE-->
+    <!-- css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/select/1.2.7/css/select.bootstrap4.min.css" />
-    
     <link rel="stylesheet" href="public/css/estils.css">
-
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <!-- js -->
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
@@ -22,17 +24,45 @@
     <script src="https://cdn.datatables.net/select/1.2.7/js/dataTables.select.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
-    
-    <!--<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>-->
+
+    <!--formden.js communicates with FormDen server to validate fields and submit via AJAX -->
+    <script type="text/javascript" src="https://formden.com/static/cdn/formden.js"></script>
+    <!-- Special version of Bootstrap that is isolated to content wrapped in .bootstrap-iso -->
+    <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css">
+    <!--Font Awesome (added because you use icons in your prepend/append)-->
+    <link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Inline CSS based on choices in "Settings" tab -->
+
+    <style>
+        .bootstrap-iso .formden_header h2,
+        .bootstrap-iso .formden_header p,
+        .bootstrap-iso form {
+            font-family: Arial, Helvetica, sans-serif;
+            color: black
+        }
+
+        .bootstrap-iso form button,
+        .bootstrap-iso form button:hover {
+            color: white !important;
+        }
+
+        .asteriskField {
+            color: red;
+        }
+    </style>
+
 
     <script>
-        $(function() {
-            $("#datepicker").datepicker({
-                dateFormat: 'yy/mm/dd'
-            });
-
-        });
+        $(document).ready(function() {
+            var date_input = $('input[name="day"]'); //our date input has the name "day"
+            var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+            date_input.datepicker({
+                format: 'yyyy/mm/dd',
+                container: container,
+                todayHighlight: true,
+                autoclose: true,
+            })
+        })
     </script>
 </head>
 
@@ -42,16 +72,40 @@
         <?php require_once "navbar.php" ?>
         <!-- Menu de navegacio -->
     </div>
-    <div class="container margin">
-        <form action="" method="POST">
-            <input name="day" type="text" id="datepicker" autocomplete="off">
-            <label for="datepicker">Date</label>
-            <input type="submit" value="SUBMIT">
-        </form>
+    <div class="bootstrap-iso">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <form action="" class="form-horizontal" method="post">
+                        <div class="form-group ">
+                            <label class="control-label col-sm-2 requiredField" for="date">
+                                Date
+                            </label>
+                            <div class="col-sm-10">
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <i class="fa fa-calendar"></i>
+                                    </div>
+                                    <input class="form-control" id="date" name="day" placeholder="YYYY/MM/DD" type="text" autocomplete="off">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-10 col-sm-offset-2">
+                                <input name="_honey" style="display:none" type="text">
+                                <button class="btn btn-primary " name="submit" type="submit">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 
     <div class="container" style="margin-bottom: 10%; margin-top: 5%;">
-        <h1>Mis Reservas(<?php echo $booking->getDate() ?>)</h1>
+        <h1>Reservas para <?php echo $booking->getDate() ?></h1>
         <table id="tableReservas" class="table table-striped table-bordered">
             <thead>
                 <tr>
@@ -62,11 +116,13 @@
         </table>
     </div>
 
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css">
 
 
     <script>
         $(document).ready(function() {
-
+            //DT
             var t = $('#tableReservas').DataTable({
                 "bPaginate": false,
                 "bLengthChange": false,
