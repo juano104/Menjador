12a6cc05ed3c4d75dc015d7b0a1ca0b928bf4624
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-23 11:47:22 +0100

    Update Read.php

--- api/Controller/Student/Read.php
+++ api/Controller/Student/Read.php
@@ -34,25 +34,25 @@ if (isset($_POST['submit'])) {
     }
 } else {
     //session_start();
-    if (isset($_SESSION["username"])) {
-        //Headers
-        include_once '../api/Model/Database.php';
-        include_once '../api/Model/User_Parent.php';
-        include_once '../api/Model/Student.php';
+    //if (isset($_SESSION["username"])) {
+    //Headers
+    include_once '../api/Model/Database.php';
+    include_once '../api/Model/User_Parent.php';
+    include_once '../api/Model/Student.php';
 
-        //DB
-        $db = new Database();
-        $db_conn = $db->connect();
+    //DB
+    $db = new Database();
+    $db_conn = $db->connect();
 
-        //User
-        $user = new User_Parent($db_conn);
+    //User
+    $user = new User_Parent($db_conn);
 
-        $user->setUsername($_SESSION["username"]);
-        $parent_DNI = $user->getUsername();
+    $user->setUsername($_SESSION["username"]);
+    $parent_DNI = $user->getUsername();
 
-        $stmt = $user->read();
-        $count = $stmt->rowCount();
+    $stmt = $user->read();
+    $count = $stmt->rowCount();
 
-        include_once "../front/View/index.php";
-    }
+    include_once "../front/View/index.php";
+    //}
 }
