b625cefb1bacfb300eb0af3608b725c2e02ea86d
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-02-23 10:32:35 +0100

    estils i navbar referencia login

--- intranet/View/View-Insert-Plate.php
+++ intranet/View/View-Insert-Plate.php
@@ -1,5 +1,5 @@
 <div class="container margin">
-  <h2>Inserir Plat</h2>
+  <h1>Inserir Plat</h1>
   <button type="button" style="margin-bottom: 2%;" class="btn btn-info" data-toggle="collapse" data-target="#demo"><i class="fas fa-utensils"></i></button>
   <div id="demo" class="collapse">
           <form action="insertarPlate" class="form-inline" style="margin: 0%;" method="post">
