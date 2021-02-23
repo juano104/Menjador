e74a7475371445d928e685eb6b742ac415c3336f
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-23 11:41:00 +0100

    Update Read.php

--- api/Controller/Student/Read.php
+++ api/Controller/Student/Read.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['submit'])) {
-    if ((isset($_POST['username']) && $_POST['username'] != '') && (isset($_POST['password']) && $_POST['password'] != '')) {
+    if ((isset($_POST['username'])) && (isset($_POST['password']))) {
 
         session_start();
         $username = trim($_POST['username']);
