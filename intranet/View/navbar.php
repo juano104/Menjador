90eb2405fdd17c89569cc58dd9cdff2eb91805df
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-02-23 18:40:55 +0100

    probasvg

--- intranet/View/navbar.php
+++ intranet/View/navbar.php
@@ -15,7 +15,7 @@
   <div class="container na">
     <a href="#" class="navbar-brand">
       <!-- Logo Image -->
-      <img src="public/img/logo.png" width="100" alt="" class="d-inline-block align-middle mr-2">
+      <img src="public/img/logo.svg" width="100" alt="" class="d-inline-block align-middle mr-2">
     </a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
