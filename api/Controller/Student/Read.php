e43fe1246e6ce2bf103246e5fa557bb0d367179b
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-23 09:56:16 +0100

    Update Read.php

--- api/Controller/Student/Read.php
+++ api/Controller/Student/Read.php
@@ -40,7 +40,7 @@ if (isset($_POST['submit'])) {
                 $errorMsg =  "Wrong Email Or Password";
             }
         }
-    } else {
+    } /*else {
         if (isset($_SESSION["name"])) {
 
             //Headers
@@ -72,7 +72,7 @@ if (isset($_POST['submit'])) {
                 }
             }
         }
-    }
+    }*/
 }
 
 
