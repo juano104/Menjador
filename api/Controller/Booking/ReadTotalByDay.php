e1c04c0d3ac244680ff76549582839f7c27e1005
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-24 13:16:39 +0100

    Update ReadTotalByDay.php

--- api/Controller/Booking/ReadTotalByDay.php
+++ api/Controller/Booking/ReadTotalByDay.php
@@ -11,8 +11,18 @@ $db_conn = $db->connect();
 
 //User
 $booking = new Booking($db_conn);
-$fecha1 = date('Y') . '-01-01';
-$fecha2 = date('Y') . '-06-21';
+
+$year = date('Y');
+if (date('n') < 6) {
+    $ayear = $year - 1;
+    $byear = $year;
+} else {
+    $ayear = $year;
+    $byear = $year + 1;
+}
+
+$fecha1 = $year . '-09-01';
+$fecha2 = $year + 1 . '-06-21';
 
 for ($i = $fecha1; $i <= $fecha2; $i = date("Y-m-d", strtotime($i . "+ 1 days"))) {
     $day = date('l', strtotime($i));
