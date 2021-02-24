e968acf226649beefc51ed7d9919c655d7a75f37
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-02-24 17:52:06 +0100

    Update ReadTotalAllergy.php

--- api/Controller/Booking/ReadTotalAllergy.php
+++ api/Controller/Booking/ReadTotalAllergy.php
@@ -24,20 +24,24 @@ if (date('n') < 6) {
 $fecha1 = $ayear . '-09-01';
 $fecha2 = $byear . '-06-21';
 
+$Alergias = array();
+
 for ($i = $fecha1; $i <= $fecha2; $i = date("Y-m-d", strtotime($i . "+ 1 days"))) {
     $day = date('l', strtotime($i));
     $dayofweek = strtolower($day);
 
     $stmt = $booking->readTotalAllergy($dayofweek, $i);
-
+    
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     extract($row);
     $e[] = array(
         "date" => $i,
         "title" => $row["title"],
+
     );
+    array_push($e, $Alergias);
 }
 
 }
-echo json_encode($e);
+echo json_encode($Alergias);
 
