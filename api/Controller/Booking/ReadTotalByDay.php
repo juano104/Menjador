7286876c95832e714c23b801c3f3adb089e616b5
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-02-24 09:32:50 +0100

    Update ReadTotalByDay.php

--- api/Controller/Booking/ReadTotalByDay.php
+++ api/Controller/Booking/ReadTotalByDay.php
@@ -12,12 +12,9 @@ $db_conn = $db->connect();
 //User
 $booking = new Booking($db_conn);
 
-
-//if (isset($_POST['day'])) {
-//$booking->setDate($_POST["day"]);
 $today = date("Y-m-d");
-$date = "2021-03-01";
-$booking->setDate($date);
+//$date = "2021-03-01";
+$booking->setDate($today);
 
 $dayname = date('l', strtotime($booking->getDate()));;
 $dayofweek = strtolower($dayname);
@@ -35,27 +32,21 @@ echo "...";
 echo $booking->getSum();
 
 require_once "View/total.php";
-//} else {
-    /*$today = date("Y-m-d");
-    $booking->setDate($today);
-
+if (isset($_POST['day'])) {
+    $booking->setDate($_POST["day"]);
     $dayname = date('l', strtotime($booking->getDate()));;
     $dayofweek = strtolower($dayname);
-    if ($dayofweek == "saturday" || $dayofweek == "sunday") {
-        echo "No reservations on weekends";
-    } else {
-        $booking->setDow($dayofweek);
+    $booking->setDow($dayofweek);
 
-        $stmt = $booking->readTotalByDay();
-        $arr = array();
+    $stmt = $booking->readTotalByDay();
+    $arr = array();
 
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        extract($row);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    extract($row);
 
-        $booking->setSum($row["sum"]);
+    $booking->setSum($row["sum"]);
 
-        echo $dayofweek;
-        echo $booking->getSum() . "not post";
-        require_once "View/total.php";
-    }
-}*/
+    echo $dayofweek;
+    echo $booking->getSum() . "not post";
+    require_once "View/total.php";
+}
