01f128c47a7fc91269c804f6b2752da7d91b33a9
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-03-01 10:48:29 +0100

    Update ReadAllByDay.php

--- api/Controller/Booking/ReadAllByDay.php
+++ api/Controller/Booking/ReadAllByDay.php
@@ -25,6 +25,7 @@ if (date('n') < 6) {
 
 $fecha1 = $ayear . '-09-01';
 $fecha2 = $byear . '-06-21';
+$arr = array();
 
 for ($i = $fecha1; $i <= $fecha2; $i = date("Y-m-d", strtotime($i . "+ 1 days"))) {
     $day = date('l', strtotime($i));
@@ -37,15 +38,27 @@ for ($i = $fecha1; $i <= $fecha2; $i = date("Y-m-d", strtotime($i . "+ 1 days"))
 
     $stmt = $booking->readAllByExtra();
 
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $e = array(
+            "date" => $i,
+            "title" => $name
+        );
+
+        array_push($arr, $e);
+    }
+
+
+
+    //$row = $stmt->fetch(PDO::FETCH_ASSOC);
     //$booking->setSum($row["title"] ?? '');
 
-    $e[] = array(
+    /*$e[] = array(
         "date" => $i,
         "title" => "Reservas: " . $row["name"],
-    );
+    );*/
 }
-echo json_encode($e);
+echo json_encode($arr);
 
 
 
