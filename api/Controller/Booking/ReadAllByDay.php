84d564df6a0a214346c9249277b734ece6992b6f
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-03-01 11:41:11 +0100

    Update ReadAllByDay.php

--- api/Controller/Booking/ReadAllByDay.php
+++ api/Controller/Booking/ReadAllByDay.php
@@ -1,63 +1,6 @@
 <?php
 
-
-//session_start();
-
-//Headers
-include_once '../api/Model/Database.php';
-include_once '../api/Model/Booking.php';
-
-//DB
-$db = new Database();
-$db_conn = $db->connect();
-
-//User
-$booking = new Booking($db_conn);
-
-$year = date('Y');
-if (date('n') < 6) {
-    $ayear = $year - 1;
-    $byear = $year;
-} else {
-    $ayear = $year;
-    $byear = $year + 1;
-}
-
-$fecha1 = $ayear . '-09-01';
-$fecha2 = $byear . '-06-21';
-
-
-for ($i = $fecha1; $i <= $fecha2; $i = date("Y-m-d", strtotime($i . "+ 1 days"))) {
-    $day = date('l', strtotime($i));
-    $dayofweek = strtolower($day);
-
-    $booking->setDow($dayofweek);
-    $booking->setDate($day);
-    $booking->setUsername("56142879E"  ?? '');
-
-    $stmt = $booking->readAllByExtra();
-
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
-        $e[] = array(
-            "date" => $i,
-            "title" => $title,
-        );
-    }
-}
-echo json_encode($e);
-
-
-
-
-
-
-
-
-
-
-
-/*session_start();
+session_start();
 
 //Headers
 include_once '../api/Model/Database.php';
@@ -87,25 +30,23 @@ for ($i = $fecha1; $i <= $fecha2; $i = date("Y-m-d", strtotime($i . "+ 1 days"))
     $dayofweek = strtolower($day);
 
     $booking->setDow($dayofweek);
-    $booking->setDate($day);
-    $booking->setUsername("79481024P");
+    $booking->setDate($i);
+    $booking->setUsername("56142879E");
 
 
     $stmt = $booking->readAllByExtra();
-    $arrextra = array();
-
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    
+    /*while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
         $e[] = array(
             "date" => $i,
             "title" => "Tus Reservas: " . $title
         );
         //array_push($arrextra, $e);
-    }
+    }*/
 
 
-
-    /*$row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
     extract($row);
     //$booking->setSum($row["title"] ?? '');
 
@@ -114,7 +55,7 @@ for ($i = $fecha1; $i <= $fecha2; $i = date("Y-m-d", strtotime($i . "+ 1 days"))
         "title" => "Reservas: " . $title,
     );
 }
-echo json_encode($e);*/
+echo json_encode($e);
 
 
 
