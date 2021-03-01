8240250db508ed8c5938930202aa3a3810e4dd0f
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-03-01 11:25:11 +0100

    Update ReadAllByDay.php

--- api/Controller/Booking/ReadAllByDay.php
+++ api/Controller/Booking/ReadAllByDay.php
@@ -1,7 +1,64 @@
 <?php
 
 
-session_start();
+//session_start();
+
+//Headers
+include_once '../api/Model/Database.php';
+include_once '../api/Model/Booking.php';
+
+//DB
+$db = new Database();
+$db_conn = $db->connect();
+
+//User
+$booking = new Booking($db_conn);
+
+$year = date('Y');
+if (date('n') < 6) {
+    $ayear = $year - 1;
+    $byear = $year;
+} else {
+    $ayear = $year;
+    $byear = $year + 1;
+}
+
+$fecha1 = $ayear . '-09-01';
+$fecha2 = $byear . '-06-21';
+
+$Alergias = array();
+
+for ($i = $fecha1; $i <= $fecha2; $i = date("Y-m-d", strtotime($i . "+ 1 days"))) {
+    $day = date('l', strtotime($i));
+    $dayofweek = strtolower($day);
+
+    $booking->setDow($dayofweek);
+    $booking->setDate($day);
+    $booking->setUsername("79481024P");
+    $stmt = $booking->readAllByExtra();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $e[] = array(
+            "date" => $i,
+            "title" => $title,
+
+        );
+    }
+}
+echo json_encode($e);
+
+
+
+
+
+
+
+
+
+
+
+/*session_start();
 
 //Headers
 include_once '../api/Model/Database.php';
@@ -56,9 +113,9 @@ for ($i = $fecha1; $i <= $fecha2; $i = date("Y-m-d", strtotime($i . "+ 1 days"))
     $e[] = array(
         "date" => $i,
         "title" => "Reservas: " . $title,
-    );*/
+    );
 }
-echo json_encode($e);
+echo json_encode($e);*/
 
 
 
