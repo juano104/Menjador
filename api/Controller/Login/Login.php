3f67e8fe6bf0796ebb960627fa42737c25296ddc
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-03-03 09:35:32 +0100

    Update Login.php

--- api/Controller/Login/Login.php
+++ api/Controller/Login/Login.php
@@ -1,40 +1,5 @@
 <?php
 
-/*//Headers
-include_once '../api/Model/Database.php';
-include_once '../api/Model/Login.php';
-
-//DB
-$db = new Database();
-$db_conn = $db->connect();
-
-
-$user = new Login($db_conn);
-
-$username = $_POST["username"];
-$user->setDNI($username);
-
-//$stmt = $user->read();
-
-$row = $user->read()->fetch(PDO::FETCH_ASSOC);
-$user->setRole($row["role"]);
-
-if ($user->getDNI() == $row["DNI"] && $user->getRole() == "admin") {
-    header('location: http://admin.menjadorescola.me/');
-    //header('location: https://www.google.com/');
-} else if ($user->getDNI() == $row["DNI"] && $user->getRole() == "restaurant") {
-    header('location: http://intranet.menjadorescola.me/');
-    //header('location: https://www.youtube.com/');
-} else if ($user->getDNI() == $row["DNI"] && $user->getRole() == "parent") {
-    header('location: http://www.menjadorescola.me/home');
-    //header('location: https://www.amazon.com/');
-} else {
-    echo "<script>
-            alert('Access Denied!');
-        </script>";
-}*/
-
-
 session_start();
 if (isset($_POST['submit'])) {
     if ((isset($_POST['username']) && $_POST['username'] != '') && (isset($_POST['password']) && $_POST['password'] != '')) {
@@ -77,34 +42,26 @@ if (isset($_POST['submit'])) {
 
         $stmt = $user->read();
         $count = $stmt->rowCount();
-        //$pass = $user->readPassword();
-        //$countpass = $pass->rowCount();
 
         if ($countpassp  == 1) {
             $row = $passparent->fetch(PDO::FETCH_ASSOC);
             extract($row);
 
             if ($passwordf == $row['password']) {
-                /*if ($login->getDNI() == $rowlog["DNI"] && $login->getRole() == "admin") {
-                    header('location: http://admin.menjadorescola.me/');
-                    //header('location: https://www.google.com/');
-                } else if ($login->getDNI() == $rowlog["DNI"] && $login->getRole() == "restaurant") {
-                    header('location: http://intranet.menjadorescola.me/');
-                    //header('location: https://www.youtube.com/');
-                } else*/
                 if ($login->getDNI() == $rowlog["DNI"] && $login->getRole() == "parent") {
                     include_once "../front/View/index.php";
-                    //header('location: http://www.menjadorescola.me/home');
-                    //header('location: https://www.amazon.com/');
-                } else {
-                    echo "<script>
-                            alert('Access Denied!');
-                        </script>";
                 }
             } else {
-                //header("Location: http://www.menjadorescola.me/");
-                echo "<script>alert('Wrong Password')</script>";
+                echo "<script>
+                alert('Wrong Password');
+                window.location.href='www.menjadorescola.me';
+                </script>";
             }
+        } else {
+            echo "<script>
+            alert('Your username or password is incorrect');
+            window.location.href='www.menjadorescola.me';
+            </script>";
         }
         if ($countpassa  == 1) {
             $row = $passadmin->fetch(PDO::FETCH_ASSOC);
@@ -113,16 +70,18 @@ if (isset($_POST['submit'])) {
             if ($passwordf == $row['password']) {
                 if ($login->getDNI() == $rowlog["DNI"] && $login->getRole() == "admin") {
                     header('location: http://admin.menjadorescola.me/');
-                    //header('location: https://www.google.com/');
-                } else {
-                    echo "<script>
-                            alert('Access Denied!');
-                        </script>";
                 }
             } else {
-                //header("Location: http://www.menjadorescola.me/");
-                echo "<script>alert('Wrong Password')</script>";
+                echo "<script>
+                alert('Wrong Password');
+                window.location.href='www.menjadorescola.me';
+                </script>";
             }
+        } else {
+            echo "<script>
+            alert('Your username or password is incorrect');
+            window.location.href='www.menjadorescola.me';
+            </script>";
         }
         if ($countpassr  == 1) {
             $row = $passrest->fetch(PDO::FETCH_ASSOC);
@@ -131,16 +90,18 @@ if (isset($_POST['submit'])) {
             if ($passwordf == $row['password']) {
                 if ($login->getDNI() == $rowlog["DNI"] && $login->getRole() == "restaurant") {
                     header('location: http://intranet.menjadorescola.me/');
-                    //header('location: https://www.youtube.com/');
-                } else {
-                    echo "<script>
-                            alert('Access Denied!');
-                        </script>";
                 }
             } else {
-                //header("Location: http://www.menjadorescola.me/");
-                echo "<script>alert('Wrong Password')</script>";
+                echo "<script>
+                alert('Wrong Password');
+                window.location.href='www.menjadorescola.me';
+                </script>";
             }
+        } else {
+            echo "<script>
+            alert('Your username or password is incorrect');
+            window.location.href='www.menjadorescola.me';
+            </script>";
         }
     }
 } else {
