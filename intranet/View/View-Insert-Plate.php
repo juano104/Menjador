7b8cacde6834e682e8baeb7289f91cafd0106164
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-03-06 19:02:02 +0100

    sdc

--- intranet/View/View-Insert-Plate.php
+++ intranet/View/View-Insert-Plate.php
@@ -5,8 +5,7 @@
     <form action="insertarPlate" class="form-inline" style="margin: 0%;" method="post" class="needs-validation" novalidate>
       <div class="form-group mb-2">
         <label for="nomplat">
-
-          <input type="text" name="nomplat" id="nomplat" required>
+          <input type="text" name="nomplat" class="form-control" id="nomplat" required>
           <div class="invalid-feedback">
             Porfavor, ponga un plato.
           </div>
@@ -28,3 +27,25 @@
   </div>
 </div>
 
+
+<script>
+            // Example starter JavaScript for disabling form submissions if there are invalid fields
+            (function() {
+                'use strict';
+                window.addEventListener('load', function() {
+                    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                    var forms = document.getElementsByClassName('needs-validation');
+                    // Loop over them and prevent submission
+                    var validation = Array.prototype.filter.call(forms, function(form) {
+                        form.addEventListener('submit', function(event) {
+                            if (form.checkValidity() === false) {
+                              document.write("funciona");
+                                event.preventDefault();
+                                event.stopPropagation();
+                            }
+                            form.classList.add('was-validated');
+                        }, false);
+                    });
+                }, false);
+            })();
+        </script>
