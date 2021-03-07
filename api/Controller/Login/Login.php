4884d3e42ca9774b40b29f555281b03a86667b2d
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-03-07 12:49:36 +0100

    Update Login.php

--- api/Controller/Login/Login.php
+++ api/Controller/Login/Login.php
@@ -64,6 +64,7 @@ if (isset($_POST['submit'])) {
             if ($passwordf == $row['password']) {
                 if ($login->getDNI() == $rowlog["DNI"] && $login->getRole() == "admin") {
                     header('location: https://admin.menjadorescola.me/');
+                    echo '<script language="javascript">alert("funciona");</script>';
                 }
             } else {
                 echo "<script>
