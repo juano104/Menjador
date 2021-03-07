788712b76a455e675995b3db19d11f3cbd7a82fc
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-03-07 13:08:10 +0100

    Update View-Booking.php

--- back/View/View-Booking.php
+++ back/View/View-Booking.php
@@ -8,7 +8,7 @@
     <title>Read_All</title>
 
     <!-- css -->
-    
+
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css" />
 
@@ -22,6 +22,8 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.7/css/responsive.dataTables.min.css">
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css"/>
 
     <!-- js -->
 
@@ -44,6 +46,11 @@
     <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
 
     <script src="https://cdn.datatables.net/responsive/2.2.7/js/dataTables.responsive.min.js"></script>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
 </head>
 
 <body>
@@ -104,7 +111,14 @@
                     url: "//cdn.datatables.net/plug-ins/1.10.19/i18n/English.json"
                 },
                 select: true,
-                responsive: true
+                responsive: true,
+                buttons: [
+                    
+                    {
+                        extend: 'pdf',
+                        text: '<i class="fas fa-file-pdf"></i>'
+                    }
+                ]
 
             });
 
@@ -127,7 +141,14 @@
                         url: "//cdn.datatables.net/plug-ins/1.10.19/i18n/English.json"
                     },
                     select: true,
-                    responsive: true
+                    responsive: true,
+                    buttons: [
+
+                        {
+                            extend: 'pdf',
+                            text: '<i class="fas fa-file-pdf"></i>'
+                        }
+                    ]
                 });
 
             }
@@ -154,7 +175,14 @@
                     url: "//cdn.datatables.net/plug-ins/1.10.19/i18n/English.json"
                 },
                 select: true,
-                responsive: true
+                responsive: true,
+                buttons: [
+
+                    {
+                        extend: 'pdf',
+                        text: '<i class="fas fa-file-pdf"></i>'
+                    }
+                ]
 
             });
 
@@ -179,7 +207,14 @@
                         url: "//cdn.datatables.net/plug-ins/1.10.19/i18n/English.json"
                     },
                     select: true,
-                    responsive: true
+                    responsive: true,
+                    buttons: [
+
+                        {
+                            extend: 'pdf',
+                            text: '<i class="fas fa-file-pdf"></i>'
+                        }
+                    ]
                 });
 
             }
