919617610cadd9f5751ca7e0e9e25b997136fd0b
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-03-09 13:50:32 +0100

    Update index.php

--- front/View/index.php
+++ front/View/index.php
@@ -318,18 +318,18 @@
                                     </div>
                                     </p>
                                     <ul class="list-unstyled list-inline text-center">
-                                        <li class="list-inline-item" style="width: 8%; height: 8%;">
-                                            <a class="btn-floating btn-fb mx-1">
+                                        <li class="list-inline-item" >
+                                            <a class="btn-floating btn-fb mx-1" style="width: 8%; height: 8%;">
                                                 <i class="fab fa-cc-visa"></i>
                                             </a>
                                         </li>
-                                        <li class="list-inline-item" style="width: 8%; height: 8%;">
-                                            <a class="btn-floating btn-tw mx-1">
+                                        <li class="list-inline-item" >
+                                            <a class="btn-floating btn-tw mx-1" style="width: 8%; height: 8%;">
                                                 <i class="fab fa-cc-mastercard"></i>
                                             </a>
                                         </li>
-                                        <li class="list-inline-item" style="width: 8%; height: 8%;">
-                                            <a class="btn-floating btn-gplus mx-1">
+                                        <li class="list-inline-item" >
+                                            <a class="btn-floating btn-gplus mx-1" style="width: 8%; height: 8%;">
                                                 <i class="fab fa-cc-paypal"></i>
                                             </a>
                                         </li>
