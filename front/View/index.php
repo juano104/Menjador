512cbeea47006c65d9212fbf02e475aacba9707d
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-03-09 18:51:24 +0100

    Update index.php

--- front/View/index.php
+++ front/View/index.php
@@ -284,7 +284,7 @@
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h3 class="modal-title" id="exampleModalLabel">Tarjeta <i class="fas fa-credit-card fa-2x"></i></h3>
+                                <h3 class="modal-title" id="exampleModalLabel">Tarjeta <i class="fas fa-credit-card fa-sm"></i></h3>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
@@ -364,7 +364,7 @@
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h3 class="modal-title" id="exampleModalLabel">Banco <i class="fas fa-university fa-2x"></i></h3>
+                                <h3 class="modal-title" id="exampleModalLabel">Banco <i class="fas fa-university fa-sm"></i></h3>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
