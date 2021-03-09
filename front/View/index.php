262c8c6c2114b298257aec7987e970d27437ace1
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-03-09 10:14:37 +0100

    Update index.php

--- front/View/index.php
+++ front/View/index.php
@@ -287,22 +287,51 @@
                                 </button>
                             </div>
                             <div class="modal-body">
-                                <h2>Formato de número de tarjeta</h2>
-                                <input type="text" class="cc-number" pattern="\d*" x-autocompletetype="cc-number" placeholder="Card number" required>
-
-                                <h2>Formato de caducidad</h2>
-
-                                <input type="text" class="cc-exp" pattern="\d*" x-autocompletetype="cc-exp" placeholder="Expires MM/YY" required maxlength="9">
-
-                                <h2>Formato CVC</h2>
-
-                                <input type="text" class="cc-cvc" pattern="\d*" x-autocompletetype="cc-csc" placeholder="Security code" required autocomplete="off">
-
-                                <h2>Restringir numérico</h2>
-
-                                <input type="text" data-numeric>
-
-                                <h2 class="validation"></h2>
+                            <fieldset>
+      <label for="card-number">Número de Tarjeta</label>
+      <input type="num" id="card-number" class="input-cart-number" maxlength="4" />
+      <input type="num" id="card-number-1" class="input-cart-number" maxlength="4" />
+      <input type="num" id="card-number-2" class="input-cart-number" maxlength="4" />
+      <input type="num" id="card-number-3" class="input-cart-number" maxlength="4" />
+    </fieldset>
+    <fieldset>
+      <label for="card-holder">Nombre</label>
+      <input type="text" id="card-holder" />
+    </fieldset>
+    <fieldset class="fieldset-expiration">
+      <label for="card-expiration-month">Fecha de Expiración</label>
+      <div class="select">
+        <select id="card-expiration-month">
+          <option></option>
+          <option>01</option>
+          <option>02</option>
+          <option>03</option>
+          <option>04</option>
+          <option>05</option>
+          <option>06</option>
+          <option>07</option>
+          <option>08</option>
+          <option>09</option>
+          <option>10</option>
+          <option>11</option>
+          <option>12</option>
+        </select>
+      </div>
+      <div class="select">
+        <select id="card-expiration-year">
+          <option></option>
+          <option>2021</option>
+          <option>2022</option>
+          <option>2023</option>
+          <option>2024</option>
+          <option>2025</option>
+        </select>
+      </div>
+    </fieldset>
+    <fieldset class="fieldset-ccv">
+      <label for="card-ccv">CCV</label>
+      <input type="text" id="card-ccv" maxlength="3" />
+    </fieldset>
 
                             </div>
                             <div class="modal-footer">
@@ -780,53 +809,59 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <script>
-        var J = Payment.J,
+        $('.input-cart-number').on('keyup change', function(){
+  $t = $(this);
+  
+  if ($t.val().length > 3) {
+    $t.next().focus();
+  }
+  
+  var card_number = '';
+  $('.input-cart-number').each(function(){
+    card_number += $(this).val() + ' ';
+    if ($(this).val().length == 4) {
+      $(this).next().focus();
+    }
+  })
+  
+  $('.credit-card-box .number').html(card_number);
+});
 
-            numeric = document.querySelector('[data-numeric]'),
+$('#card-holder').on('keyup change', function(){
+  $t = $(this);
+  $('.credit-card-box .card-holder div').html($t.val());
+});
 
-            number = document.querySelector('.cc-number'),
+$('#card-holder').on('keyup change', function(){
+  $t = $(this);
+  $('.credit-card-box .card-holder div').html($t.val());
+});
 
-            exp = document.querySelector('.cc-exp'),
+$('#card-expiration-month, #card-expiration-year').change(function(){
+  m = $('#card-expiration-month option').index($('#card-expiration-month option:selected'));
+  m = (m < 10) ? '0' + m : m;
+  y = $('#card-expiration-year').val().substr(2,2);
+  $('.card-expiration-date div').html(m + '/' + y);
+})
 
-            cvc = document.querySelector('.cc-cvc'),
+$('#card-ccv').on('focus', function(){
+  $('.credit-card-box').addClass('hover');
+}).on('blur', function(){
+  $('.credit-card-box').removeClass('hover');
+}).on('keyup change', function(){
+  $('.ccv div').html($(this).val());
+});
 
-            validation = document.querySelector('.validation');
 
-        Payment.restrictNumeric(numeric);
+/*--------------------
+CodePen Tile Preview
+--------------------*/
+setTimeout(function(){
+  $('#card-ccv').focus().delay(1000).queue(function(){
+    $(this).blur().dequeue();
+  });
+}, 500);
 
-        Payment.formatCardNumber(number);
-
-        Payment.formatCardExpiry(exp);
-
-        Payment.formatCardCVC(cvc);
-
-        document.querySelector('form').onsubmit = function(e) {
-
-            e.preventDefault();
-
-            J.toggleClass(document.querySelectorAll('input'), 'invalid');
-
-            J.removeClass(validation, 'passed failed');
-
-            var cardType = Payment.fns.cardType(J.val(number));
-
-            J.toggleClass(number, 'invalid', !Payment.fns.validateCardNumber(J.val(number)));
-
-            J.toggleClass(exp, 'invalid', !Payment.fns.validateCardExpiry(Payment.cardExpiryVal(exp)));
-
-            J.toggleClass(cvc, 'invalid', !Payment.fns.validateCardCVC(J.val(cvc), cardType));
-
-            if (document.querySelectorAll('.invalid').length) {
-
-                J.addClass(validation, 'failed');
-
-            } else {
-
-                J.addClass(validation, 'passed');
-
-            }
-
-        }
     </script>
 
 </body>
