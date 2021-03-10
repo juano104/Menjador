e39ce136b531aa7d4dde01d9058e1f2223e6c1e0
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-03-10 13:25:49 +0100

    Update View-Reservas.php

--- back/View/View-Reservas.php
+++ back/View/View-Reservas.php
@@ -95,6 +95,7 @@ if ($_SESSION["username"] == "") {
             <div class="row">
                 <div class="col-md-6 col-sm-6 col-xs-12">
                     <!--<h3>Selecciona una fecha</h3>-->
+                    <label for="day">Seleccione Fecha:</label>
                     <form action="" class="form-horizontal" method="post" style="margin: 0;">
                         <div class="form-group ">
                             <div class="col-sm-10">
@@ -102,8 +103,7 @@ if ($_SESSION["username"] == "") {
                                     <div class="input-group-addon">
                                         <i class="fa fa-calendar"></i>
                                     </div>
-                                    <label for="datepicker2">Seleccione Fecha:</label> <br>
-                                    <input autocomplete="off" name="day" type="text" class="datepicker">
+                                    <input autocomplete="off" name="day" type="text" id="day" class="datepicker">
                                 </div>
                             </div>
 
