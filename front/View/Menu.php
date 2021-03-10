0cc876bba74a5758d287c290ffd79349796e3c65
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-03-10 09:05:15 +0100

    Update Menu.php

--- front/View/Menu.php
+++ front/View/Menu.php
@@ -92,15 +92,15 @@
                     <h1>Ubicacion</h1>
                     <div id="mapid" style="width:400px;height:300px;"></div>
                     <script>
-                        var map = L.map('map').setView([51.505, -0.09], 13);
-
-                        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        }).addTo(map);
-
-                        L.marker([51.5, -0.09]).addTo(map)
-                            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-                            .openPopup();
+                        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+                        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                            maxZoom: 18,
+                            id: 'mapbox/streets-v11',
+                            tileSize: 512,
+                            zoomOffset: -1,
+                            accessToken: 'your.mapbox.access.token'
+                        }).addTo(mymap);
                     </script>
                 </div>
                 <!-- Social buttons -->
