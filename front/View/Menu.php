9df7abe43478c7f474d05038e1033cd6130721e3
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-03-10 11:22:42 +0100

    sess front

--- front/View/Menu.php
+++ front/View/Menu.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Menu</title>
     <link rel="stylesheet" href="public/css/estils.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link href='public/css/fullcalendar.min.css' rel='stylesheet' />
