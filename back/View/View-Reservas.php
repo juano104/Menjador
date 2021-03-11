30915e0454672046348df6986f4565a78cda1e5a
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2021-03-11 19:36:19 +0100

    footer

--- back/View/View-Reservas.php
+++ back/View/View-Reservas.php
@@ -240,73 +240,7 @@ if ($_SESSION["username"] == "") {
             }
         });
     </script>
-    <footer class="page-footer font-small stylish-color-dark pt-4">
-        <!-- Footer Links -->
-        <div class="container text-center text-md-left">
-            <!-- Grid row -->
-            <div class="row">
-                <hr class="clearfix w-100 d-md-none">
-                <!-- Grid column -->
-                <div class="col-md-2 mx-auto">
-                    <!-- Links -->
-                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#!">Link 1</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 2</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 3</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 4</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- Grid column -->
-                <div class="col-md-4 mx-auto">
-                    <!-- Content -->
-                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Footer Content</h5>
-                    <p>
-                        Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
-                        consectetur
-                        adipisicing elit.
-                    </p>
-                </div>
-                <!-- Social buttons -->
-                <div class="col-md-2 mx-auto">
-                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4 text-center">Xarxes</h5>
-                    <br>
-                    <ul class="list-unstyled list-inline text-center">
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-fb mx-1">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-tw mx-1">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="btn-floating btn-gplus mx-1">
-                                <i class="fab fa-google-plus-g"></i>
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- Social buttons -->
-                </div>
-            </div>
-            <!-- Grid row -->
-            <!-- Copyright -->
-            <div class="footer-copyright text-center py-3">
-                © 2020 Copyright:
-                <a href="https://mdbootstrap.com/">Music</a>
-            </div>
-            <!-- Copyright -->
-    </footer>
+     <?php require_once "footer.php" ?>
 </body>
 
 </html>
