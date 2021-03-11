f31ce2863a6b4f8394bc9df928fc4b991f7cd367
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-03-11 10:43:36 +0100

    sess

--- front/View/login.php
+++ front/View/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(isset($_SESSION(["username"]))){
+if(isset($_SESSION["username"])){
     header("Location: https://www.menjadorescola.me/login");
 }
 ?>
--- intranet/View/login.php
+++ intranet/View/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(isset($_SESSION(["username"]))){
+if(isset($_SESSION["username"])){
     header("Location: https://intranet.menjadorescola.me/home");
 }
 ?>
