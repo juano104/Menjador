e4ed0cc7136107765f2dce10fb004893d79e1c29
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2021-03-11 17:54:23 +0100

    nav

--- front/View/Menu.php
+++ front/View/Menu.php
@@ -68,10 +68,14 @@
     <!-- Footer -->
     <?php require_once "footer.php" ?>
     <!-- Footer -->
-    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css">
+
+    <!-- Footer -->
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    -->
+
     <script>
         $("#print").click(function() {
             //#AEFC is my div for FullCalendar
